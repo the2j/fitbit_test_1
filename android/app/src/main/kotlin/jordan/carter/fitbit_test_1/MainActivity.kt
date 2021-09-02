dcf1ea9cac79e19c7a1fbb02d088ec9e0c9103bd
Jordan Carter <jordan.acarter1999@gmail.com>
2021-09-02 15:33:29 +1000

    Initial commit

---
+++ android/app/src/main/kotlin/jordan/carter/fitbit_test_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package jordan.carter.fitbit_test_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/strings.dart
@@ -0,0 +1,27 @@
+class Strings {
+  // App name
+  static const String appName = "Fitbit Example";
+
+  // Route names
+  static const String homeScreenRouteName = '/home';
+  static const String fitbitSettingsScreenRouteName = '/home/fitbitSettings';
+
+  // Screen names
+  static const String homeScreenName = "Fitbit Test import";
+  static const String fitbitSettingsScreenName = "Fitbit Account";
+
+  // Fitbit Client ID
+  static const String fitbitClientID = '239YW4';
+
+  // Fitbit Client Secret
+  static const String fitbitClientSecret = '11682ccc54706712f1a1c7e8cfc57a5e';
+
+  /// Auth Uri
+  static const String fitbitRedirectUri = 'https://43948f3e1a2ba6853b318a4979ac973b.m.pipedream.net/';
+
+  /// Callback scheme (like http://)
+  static const String fitbitCallbackScheme = 'https://';
+
+  // Placeholders
+  static const String hello = "Hello, World!";
+}
